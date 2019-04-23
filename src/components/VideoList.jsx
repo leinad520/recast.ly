f7945d96090a32c54b84269dadaffcf70dff2bec
Daniel Sangyoon Kim <ksydaniel@gmail.com>
2019-04-23 16:00:31 -0700

    save

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,9 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+import exampleVideoData from '../data/exampleVideoData.js'
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(el => <VideoListEntry video={el} />)}
   </div>
 );
 
